2431f67237b94ec7f57dd8d54943bed514e7d442
Hareesh S <105427651+HarSen0604@users.noreply.github.com>
2024-02-08 23:48:15 +0530

    Ring Election

--- Expt2/ringElection.c
+++ Expt2/ringElection.c
@@ -1,241 +1,135 @@
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <pthread.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
 #include <errno.h>
 
-struct sockaddr_in abc, sender, tosend;
-int cordinator, max, mynode;
-int soc;
+#define MSG_CONFIRM 0
+#define TRUE 1
+#define FALSE 0
 
-struct msg_node
+typedef struct list
 {
-    int ar[6];
-    char msg[100];
-    int node_no;
-};
+    int size;
+    int arr[20];
+} list;
 
-void meelc(void* arg)
-{
-    struct msg_node *tem = (struct msg_node *)arg;
-    char elect_inti[100] = "elect_inti";
-    char me_elected[100] = "me_elected";
-    socklen_t len = sizeof(sender);
-    tem->ar[mynode] = 1;
-    tem->node_no = mynode;
-    strcpy(tem->msg, elect_inti);
-    for (int i = mynode + 1; i <= max; i++)
+int connect_to_port(int connect_to) {
+    int sock_id = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock_id < 0)
     {
-        tosend.sin_port = htons(i + 4500);
-        printf("sending to %d\n", i);
-        strcpy(tem->msg, elect_inti);
-        tem->node_no = mynode;
-        sendto(soc, tem, sizeof(struct msg_node), 0, (struct sockaddr *)&tosend, len);
-        sleep(8);
+        perror("Unable to create a socket");
+        exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < mynode; i++)
+    struct sockaddr_in server_address;
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(connect_to);
+
+    // Bind the socket to a specific address and port
+    if (bind(sock_id, (const struct sockaddr *)&server_address, sizeof(server_address)) < 0)
     {
-        tosend.sin_port = htons(i + 4500);
-        strcpy(tem->msg, elect_inti);
-        tem->node_no = mynode;
-        sendto(soc, tem, sizeof(struct msg_node), 0, (struct sockaddr *)&tosend, len);
-        printf("sending to %d\n", i);
-        sleep(8);
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return sock_id;
+}
+
+void send_to_id(int id, int sock_id, list llist) {
+    struct sockaddr_in client_address;
+    memset(&client_address, 0, sizeof(client_address));
+
+    client_address.sin_family = AF_INET;
+    client_address.sin_addr.s_addr = INADDR_ANY;
+    client_address.sin_port = htons(id);
+
+    sendto(sock_id, &llist, sizeof(list), MSG_CONFIRM, (const struct sockaddr *)&client_address, sizeof(client_address));
+}
+
+void election(int id, int self, int next) {
+    list llist;
+    llist.arr[0] = self;
+    llist.size = 1;
+    printf("Forwading to: %d\n", next);
+    send_to_id(next, id, llist);
+}
+
+void print(list l) {
+    for (int i = 0; i < l.size; i++) {
+        printf("%d ", l.arr[i]);
     }
 }
 
-void *cord()
-{
-    printf("cordni = %d", cordinator);
-    socklen_t len = sizeof(sender);
-    struct msg_node *tem = malloc(sizeof(struct msg_node));
-    strcpy(tem->msg, "me_elected");
-    tem->node_no = cordinator;
-    for (int i = 0; i <= max; i++)
+int main(int argc, char *argv[]) {
+    int self = atoi(argv[1]), next = atoi(argv[3]);
+
+    bool start_at = atoi(argv[2]) == 1 ? true : false;
+
+    printf("Creating a node at %d %d\n", self, start_at);
+    int sock_id = connect_to_port(self);
+
+    if (start_at)
     {
-        tosend.sin_port = htons(i + 4500);
-        sendto(soc, tem, sizeof(struct msg_node), 0, (struct sockaddr *)&tosend, len);
-    }
-}
-
-void *fun()
-{
-    // printf("sdcv");
-    struct msg_node *tem = malloc(sizeof(struct msg_node));
-    socklen_t len = sizeof(sender);
-    char elect_inti[100] = "elect_inti";
-    char me_elected[100] = "me_elected";
-    int inpt;
-    while (1)
-    {
-        printf("enter 1 to check cordinator: ");
-        scanf("%d", &inpt);
-        if (inpt == 1)
-        {
-            tosend.sin_port = htons(cordinator + 4500);
-            strcpy(tem->msg, "alive");
-            tem->node_no = mynode;
-            sendto(soc, tem, sizeof(struct msg_node), 0, (struct sockaddr *)&tosend, len);
-            // recvfrom(soc,tem,sizeof(struct msg_node),0,(struct sockaddr *)&sender,&len);
-            sleep(8);
-            if (strcmp(tem->msg, "salive") != 0)
-            {
-                printf("\ndead cordinator\n");
-                for (int i = 0; i <= max; i++)
-                {
-                    tem->ar[mynode] = 0;
-                }
-                tem->ar[mynode] = 1;
-                tem->node_no = mynode;
-                strcpy(tem->msg, elect_inti);
-                for (int i = mynode + 1; i <= max; i++)
-                {
-                    tosend.sin_port = htons(i + 4500);
-                    printf("sending to %d\n", i);
-                    strcpy(tem->msg, elect_inti);
-                    tem->node_no = mynode;
-                    sendto(soc, tem, sizeof(struct msg_node), 0, (struct sockaddr *)&tosend, len);
-                    sleep(8);
-                }
-
-                for (int i = 0; i <= mynode; i++)
-                {
-                    tosend.sin_port = htons(i + 4500);
-                    printf("sending to %d\n", i);
-                    strcpy(tem->msg, elect_inti);
-                    tem->node_no = mynode;
-                    sendto(soc, tem, sizeof(struct msg_node), 0, (struct sockaddr *)&tosend, len);
-                    sleep(8);
-                }
-            }
-        }
-    }
-    free(tem);
-}
-
-int main(int argc, char *argv[])
-{
-    struct timeval timeout;
-    pthread_t thread1, t2 = 0, t3;
-    int a;
-    cordinator = atoi(argv[2]);
-    max = cordinator;
-    mynode = atoi(argv[1]);
-    soc = socket(AF_INET, SOCK_DGRAM, 0);
-    timeout.tv_sec = 5;
-    timeout.tv_usec = 0;
-    abc.sin_addr.s_addr = INADDR_ANY;
-    abc.sin_family = AF_INET;
-    abc.sin_port = htons(atoi(argv[1]) + 4500);
-    bind(soc, (struct sockaddr *)&abc, sizeof(abc));
-    setsockopt(soc, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout));
-    tosend.sin_addr.s_addr = INADDR_ANY;
-    tosend.sin_family = AF_INET;
-    tosend.sin_port = htons(cordinator + 4500);
-    struct msg_node *tem = malloc(sizeof(struct msg_node));
-    char elect_inti[100] = "elect_inti";
-    char me_elected[100] = "me_elected";
-    socklen_t len = sizeof(sender);
-    pthread_create(&thread1, NULL, fun, NULL);
-    while (1)
-    {
-        ssize_t bytesRead = recvfrom(soc, tem, sizeof(struct msg_node), 0, (struct sockaddr *)&sender, &len);
-        if (bytesRead == -1)
-        {
-            if (errno == EWOULDBLOCK || errno == EAGAIN)
-            {
-                // Handle timeout (no data received within the specified time)
-                printf("\nTimeout: No data received within the specified time.mynode = %d cord = %d\n", mynode, cordinator);
-            }
-            continue;
-        }
-        printf("%s\n", tem->msg);
-        if (strcmp(elect_inti, tem->msg) == 0)
-        {
-            // if(tem->node_no==mynode){
-            strcpy(tem->msg, "OK");
-            tosend.sin_port = htons(tem->node_no + 4500);
-            printf(" sending okay to %d node\n ", tem->node_no);
-            tem->node_no = mynode;
-            sendto(soc, tem, sizeof(struct msg_node), 0, (struct sockaddr *)&tosend, len);
-            strcpy(tem->msg, "O");
-            for (int i = 0; i <= max; i++)
-            {
-                if (tem->ar[i] == 1)
-                {
-                    a = i;
-                    break;
-                }
-            }
-
-            if (a == mynode)
-            {
-                for (int i = 0; i <= max; i++)
-                {
-                    if (tem->ar[i] == 1)
-                    {
-                        a = i;
-                    }
-                }
-                cordinator = a;
-                printf("cordinator %d\n", a);
-                pthread_create(&t3, NULL, cord, NULL);
-                continue;
-            }
-            tem->node_no = mynode;
-            pthread_create(&t2, NULL, meelc, (void *)tem);
-            //}
-        }
-
-        if (strcmp(me_elected, tem->msg) == 0)
-        {
-            if (t2 != 0)
-            {
-                pthread_cancel(t2);
-                t2 = 0;
-            }
-            cordinator = tem->node_no;
-            pthread_cancel(thread1);
-            pthread_create(&thread1, NULL, fun, NULL);
-        }
-
-        if (strcmp("alive", tem->msg) == 0)
-        {
-            printf("killing\n");
-            tosend.sin_port = htons(tem->node_no + 4500);
-            strcpy(tem->msg, "salive");
-            tem->node_no = mynode;
-            sendto(soc, tem, sizeof(struct msg_node), 0, (struct sockaddr *)&tosend, len);
-        }
-
-        if (strcmp("salive", tem->msg) == 0)
-        {
-            printf("killing as alive \n");
-            pthread_cancel(thread1);
-            pthread_create(&thread1, NULL, fun, NULL);
-        }
-
-        if (strcmp(tem->msg, "OK") == 0)
-        {
-
-            printf("killing as ok recieved from %d\n", tem->node_no);
-            pthread_cancel(thread1);
-            pthread_create(&thread1, NULL, fun, NULL);
-            if (t2 != 0)
-            {
-                pthread_cancel(t2);
-                t2 = 0;
-            }
-        }
+        election(sock_id, self, next);
     }
 
-    // strcpy(msg,"abcd");
+    struct sockaddr_in from;
+    int len;
+    list l;
 
-    // sendto(soc,tem,sizeof(struct msg_node),0,(struct sockaddr *)&sender,len);
+    while (true) {
+        memset(&from, 0, sizeof(from));
+        int n = recvfrom(sock_id, &l, sizeof(list), MSG_WAITALL, (struct sockaddr *)&from, &len);
+
+        if (l.arr[0] == -1) {
+            printf("New Co - Ordinator: %d\n", l.arr[1]);
+            if (l.arr[2] == next) {
+                break;
+            }
+            send_to_id(next, sock_id, l);
+            break;
+        }
+        else if (l.arr[0] == self) {
+            int max = l.arr[0];
+            for (int i = 1; i < l.size; i++) {
+                if (l.arr[i] > max)
+                    max = l.arr[i];
+            }
+
+            l.arr[0] = -1;
+            l.arr[1] = max;
+            l.arr[2] = self;
+
+            printf("New Co-ordinator: %d\n", l.arr[1]);
+
+            send_to_id(next, sock_id, l);
+            break;
+        }
+
+        else
+        {
+            printf("Received list: ");
+            print(l);
+            printf("\n\n");
+
+            l.arr[l.size] = self;
+            l.size++;
+
+            printf("Sending: ");
+            print(l);
+            printf(" To %d\n\n", next);
+
+            send_to_id(next, sock_id, l);
+        }
+    }
+    return 0;
 }
-
-// Hey sollu if any errors ,mostly fine than edachina solllu daaaaaaaaaaaaaa
